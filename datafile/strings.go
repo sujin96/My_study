ae9ef5e56565be56be65282f5654fd8a19952714
leesujin <jean0518@naver.com>
2021-08-21 18:29:51 +0900

    파일을 읽어들이는 함수를 패키지화

---
+++ datafile/strings.go
@@ -0,0 +1,29 @@
+//datafile 패키지는 파일에서 데이터 샘플을 읽어 옵니다.
+package datafile
+
+import (
+	"bufio"
+	"os"
+)
+
+//GetStrings는 파일의 각 줄을 문자열로 읽어 옵니다.
+func GetStrings(fileName string) ([]string, error) {
+	var lines []string
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	err = file.Close()
+	if err != nil {
+		return nil, err
+	}
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+	return lines, nil
+}
